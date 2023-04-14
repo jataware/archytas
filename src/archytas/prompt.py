d03706f416b0bf9d542f5a161c119fb1a5eb7b2c
David Samson <david.andrew.engineer@gmail.com>
2023-04-14 02:32:56 -0400

    removed unused old stuff

--- src/archytas/prompt.py
+++ src/archytas/prompt.py
@@ -2,6 +2,7 @@ from archytas.tools import get_tool_prompt_description
 
 
 prelude = 'You are the ReAct (Reason & Action) assistant. You act as an interface between a user and the system. Your job is to help the user to complete their tasks.'
+
 tool_intro = '# Tools\nYou have access to the following tools which can help you in your job:'
 
 system_tools = f"""
@@ -54,90 +55,3 @@ def build_prompt(tools: list):
     return '\n\n'.join(chunks)
 
         
-
-# prompt = f"""
-# You are the ReAct (Reason & Action) assistant. You act as an interface between a user and the system. Your job is to help the user to complete their tasks.
-
-# # Tools
-# You have access to the following tools which can help you in your job:
-
-# - calculator
-#     - the calculator can be used to perform simple arithmetic operations
-#     - _input_: a valid arithmetic expression with two operands and one operation (+ - * / ^ %)
-#     - _output_: the result of the arithmetic expression
-
-# # - search
-# #     - the search tool can be used to search the internet
-# #     - _input_: a search query
-# #     - _output_: the text of the first result from the search query (in English)
-
-
-# - datetime
-#     - the date tool can be used to get the current date
-#     - _input_: a json object with the following fields:
-#     {{
-#         "format":   # (Optional) e.g. "YYYY-MM-DD" or "YYYY-MM-DD HH:MM:SS". Must be a valid format string for the python datetime.strftime function
-#         "timezone": # (Optional) e.g. "America/New_York" or "UTC". Must be a valid timezone string for the python pytz.timezone function
-#     }}
-#     - _output_: the current date and time in the format YYYY-MM-DD HH:MM:SS. Time zone is UTC by default.
-
-# - timestamp
-#     - the timestamp tool can be used to get the current unix timestamp as a floating point number of seconds since January 1, 1970
-#     - _input_: None
-#     - _output_: the current unix timestamp as a floating point number of seconds since January 1, 1970
-    
-# - ask_user
-#     - the ask_user tool can be used to ask the user a question. You should ask the user a question if you do not have enough information to complete the task, and there is no suitable tool to help you.
-#     - _input_: a question to ask the user
-#     - _output_: the user's response to the question
-
-# - final_answer
-#     - the final_answer tool is used to indicate that you have completed the task. You should use this tool to communicate the final answer to the user.
-#     - _input_: the final answer to the user's task
-
-# - fail_task
-#     - the fail_task tool is used to indicate that you have failed to complete the task. You should use this tool to communicate the reason for the failure to the user. Do not call this tool unless you have given a good effort to complete the task.
-#     - _input_: the reason for the failure
-
-
-
-# Every response you generate should EXACTLY follow this JSON format:
-
-# {{
-#   "thought"    : # you should always think about what you need to do
-#   "tool"       : # the name of the tool.  This must be one of: [search, calculator, ask_user, final_answer, fail_task]
-#   "tool_input" : # the input to the tool
-# }}
-
-# Do not include any text outside of this JSON object. The user will not be able to see it. You can communicate with the user through the "thought" field or the ask_user tool.
-# The tool input must be a valid JSON blob (i.e. null, string, number, boolean, array, or object). The input type will depend on which tool you select, so make sure to follow the instructions for each tool.
-
-# For example, if the user asked you what the square-root of 2, you would use the calculator like so:
-# {{
-#     "thought": "I need to use the calculator to find the square-root of 2.",
-#     "tool": "calculator",
-#     "tool_input": "2^0.5"
-# }}
-
-# # Notes
-# - assume any time based knowledge you have is out of date, and should be looked up. Things like the current date, current world leaders, celebrities ages, etc.
-# - You are not very good at arithmetic, so you should generally use tools to do arithmetic for you.
-# - The user cannot see your thoughts. If you want to communicate to tell the user something, it should be via the ask_user or final_answer tools.
-# """
-
-
-
-# #@tool decorator
-# """
-# read the interface of the tool
-# include the doc comment as part of the tool
-# receive a function for converting the input (json?) to the appropriate arguments for the tool
-# give an example usage of the tool input
-# - word description of the query that would cause the tool to be used like this. e.g. "find the square root of 2"
-# - the input to the tool. e.g. "2^0.5"
-# should be able to work with a class that has multiple methods? and then each tool name is class.method
-# """
-
-# class Prompt:
-#     def __init__(self, tools):
-#         ...
--- src/archytas/test.py
+++ src/archytas/test.py
@@ -1,26 +1,23 @@
 from archytas.react import ReActAgent, FailedTaskError
-# from archytas.tools import calculator, ask_user, datetime, timestamp
 from archytas.tools import ask_user, datetime, timestamp, fib_n, example_tool, calculator
-
 
 from rich import traceback, print; traceback.install()
 from easyrepl import REPL
-history_file = 'chat_history.txt'
-
 
 import pdb
 
 
-
 def main():
 
+    # make a list of the tools to use
     # tools = [calculator, ask_user, datetime, timestamp]
     tools = [ask_user, datetime, timestamp, fib_n, example_tool, calculator]
 
+    # create the agent
     agent = ReActAgent(tools=tools, verbose=True)
 
-    for query in REPL(history_file=history_file):
-        if not query: continue
+    # run the REPL
+    for query in REPL(history_file='chat_history.txt'):
         try:
             answer = agent.react(query)
             print(f'[green]{answer}[/green]')
