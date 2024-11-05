de4745fa65db64da05ab8b2b2c79b0f8226d36f9
David Samson <david.andrew.engineer@gmail.com>
2024-11-05 14:39:47 -0500

    got structured types working for list[T]

--- archytas/structured_data_utils.py
+++ archytas/structured_data_utils.py
@@ -4,7 +4,13 @@ if TYPE_CHECKING:
     from _typeshed import DataclassInstance  # only available to type checkers
 from dataclasses import is_dataclass, asdict, _MISSING_TYPE
 
-from .archytypes import normalize_type, NormalizedType, Dataclass_t, PydanticModel_t, is_structured_type
+from .archytypes import (
+    normalize_type, NormalizedType,
+    Dataclass_t, PydanticModel_t,
+    List_t, Dict_t, Tuple_t, Union_t,
+    NotProvided,
+    is_structured_type
+)
 from .constants import TAB
 
 
@@ -32,7 +38,7 @@ def verify_model_fields(cls: type[BaseModel], data: dict) -> dict:
     return data
 
 
-def construct_structured_type(arg_type: NormalizedType, data: dict) -> 'DataclassInstance|BaseModel':
+def construct_structured_type(arg_type: NormalizedType, data: dict) -> 'DataclassInstance|BaseModel|list':
     """
     construct an instance of a structured type from a dictionary (currently only support dataclass and pydantic models)
     recursively construct nested structured types, and validate the input data against the structured type
@@ -51,6 +57,13 @@ def construct_structured_type(arg_type: NormalizedType, data: dict) -> 'Dataclas
     if isinstance(arg_type, PydanticModel_t):
         verify_model_fields(arg_type.cls, data)
         return arg_type.cls(**data)
+
+    if isinstance(arg_type, List_t):
+        assert isinstance(data, list), f"Expected list, got {type(data)} for input {data}"
+        assert not isinstance(arg_type.element_type, NotProvided), f"INTERNAL ERROR: {arg_type} is not a structured type"
+        return [construct_structured_type(arg_type.element_type, item) for item in data]
+
+
 
     # Future support for other structured types can be added here
     raise ValueError(f"Unsupported structured type {arg_type}")
@@ -102,6 +115,12 @@ def get_structured_input_description(arg_type: NormalizedType, arg_name: str, ar
 
     if isinstance(arg_type, PydanticModel_t):
         return get_pydantic_input_description(arg_type, arg_name, arg_desc, arg_default, indent=indent)
+
+    if isinstance(arg_type, Tuple_t):
+        return get_tuple_structured_input_description(arg_type, arg_name, arg_desc, arg_default, indent=indent)
+
+    if isinstance(arg_type, (List_t, Union_t, Dict_t)):
+        return get_composite_structured_input_description(arg_type, arg_name, arg_desc, arg_default, indent=indent)
 
     raise ValueError(f"Unsupported structured type {arg_name}: {arg_type}")
 
@@ -241,3 +260,87 @@ def get_pydantic_input_description(arg_type: PydanticModel_t, arg_name: str, arg
     chunks.append(f"\n{TAB*indent}}}")
 
     return chunks
+
+
+
+def get_composite_structured_input_description(arg_type: List_t | Dict_t | Union_t, arg_name: str, arg_desc: str, arg_default: str, *, indent: int) -> list[str]:
+    """
+    Build the input description for a composite structured type (List, Dict, or Union), including all of its fields (and potentially their nested fields).
+
+    Args:
+        arg_type (List_t|Dict_t|Union_t): The composite structured type to get the input description for
+        arg_name (str): The name of the argument
+        arg_desc (str): The description of the argument
+        arg_default (str): The default value of the argument
+        indent (int): The level of indentation to use for the description
+
+    Returns:
+        list[str]: A list of strings that make up the input description for the composite structured type
+    """
+    chunks = []
+    structs = get_all_nested_structured_types(arg_type)
+
+    chunks.append(f'\n{TAB*indent}"{arg_name}":')
+    if arg_desc:
+        chunks.append(f" {arg_desc}.")
+    if arg_default:
+        chunks.append(f" Defaults to {arg_default}.")
+    chunks.append(f" Input should be {arg_type} where:\n")
+    for struct in structs:
+        if isinstance(struct, Dataclass_t):
+            chunks.extend(get_dataclass_input_description(struct, f'{struct}', '', '', indent=indent+1))
+        elif isinstance(struct, PydanticModel_t):
+            chunks.extend(get_pydantic_input_description(struct, f'{struct}', '', '', indent=indent+1))
+        else:
+            raise ValueError(f"INTERNAL ERROR: get_all_nested_structured_types is only supposed to return Dataclass_t and PydanticModel_t instances. Found '{struct}'")
+
+    return chunks
+
+
+def get_tuple_structured_input_description(arg_type: Tuple_t, arg_name: str, arg_desc: str, arg_default: str, *, indent: int) -> list[str]:
+    raise NotImplementedError("Tuple structured types are not yet supported")
+
+
+
+def get_all_nested_structured_types(arg_type: NormalizedType) -> set[Dataclass_t|PydanticModel_t]:
+    """
+    Collect all Dataclass_t and PydanticModel_t types that may be nested within a composite structured type.
+    
+    Args:
+        arg_type (NormalizedType): The composite structured type to collect nested types from
+
+    Returns:
+        set[Dataclass_t|PydanticModel_t]: A set of all nested Dataclass_t and PydanticModel_t types
+    """
+    s: set[Dataclass_t|PydanticModel_t] = set()
+
+    # Dataclass_t and PydanticModel_t are what we are looking for
+    if isinstance(arg_type, Dataclass_t) or isinstance(arg_type, PydanticModel_t):
+        s.add(arg_type)
+
+    # Tuple_t look at any of the component types that are not ellipsis
+    if isinstance(arg_type, Tuple_t):
+        if not isinstance(arg_type.component_types, NotProvided):
+            for t in arg_type.component_types: 
+                if t != ...:
+                    s.update(get_all_nested_structured_types(t)) 
+
+    # List_t look at the element type
+    if isinstance(arg_type, List_t):
+        if not isinstance(arg_type.element_type, NotProvided):
+            s.update(get_all_nested_structured_types(arg_type.element_type))
+
+    # Dict_t look at the key and value types
+    if isinstance(arg_type, Dict_t):
+        if not isinstance(arg_type.key_type, NotProvided):
+            s.update(get_all_nested_structured_types(arg_type.key_type))
+        if not isinstance(arg_type.value_type, NotProvided):
+            s.update(get_all_nested_structured_types(arg_type.value_type))
+
+    # Union_t look at all the types
+    if isinstance(arg_type, Union_t):
+        if not isinstance(arg_type.types, NotProvided):
+            for t in arg_type.types:
+                s.update(get_all_nested_structured_types(t))
+
+    return s
