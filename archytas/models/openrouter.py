f345cf8addd1ad177f6658c1e0e61cea22453541
David Samson <david.andrew.engineer@gmail.com>
2025-09-17 12:24:40 -0400

    type hinting so that if the user doesn't have tools, they don't have to deal with OpenRouterToolResponse at all

--- archytas/models/openrouter.py
+++ archytas/models/openrouter.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 import requests
 import json
-from typing import Generator, Literal, overload, TypedDict, Any, Optional, Sequence, cast
+from typing import Generator, Literal, overload, TypedDict, Any, Optional, Sequence, cast, Generic, TypeVar
 from typing_extensions import NotRequired
 from pathlib import Path
 from functools import lru_cache
@@ -80,9 +80,13 @@ class Model:
 
 
     @overload
-    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[False]=False, tools:list|None=None, **kwargs) -> str | OpenRouterToolResponse: ...
+    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[False]=False, tools:None=None, **kwargs) -> str: ...
     @overload
-    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[True], tools:list|None=None, **kwargs) -> Generator[str | OpenRouterToolResponse, None, None]: ...
+    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[False]=False, tools:list, **kwargs) -> str | OpenRouterToolResponse: ...
+    @overload
+    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[True], tools:None=None, **kwargs) -> Generator[str, None, None]: ...
+    @overload
+    def complete(self, messages: list[OpenRouterMessage], *, stream:Literal[True], tools:list, **kwargs) -> Generator[str | OpenRouterToolResponse, None, None]: ...
     def complete(self, messages: list[OpenRouterMessage], *, stream:bool=False, tools:list|None=None, **kwargs) -> str | OpenRouterToolResponse | Generator[str | OpenRouterToolResponse, None, None]:
         if stream:
             return self._streaming_complete(messages, tools, **kwargs)
@@ -90,6 +94,10 @@ class Model:
             return self._blocking_complete(messages, tools, **kwargs)
 
 
+    @overload
+    def _blocking_complete(self, messages: list[OpenRouterMessage], tools:None=None, **kwargs) -> str: ...
+    @overload
+    def _blocking_complete(self, messages: list[OpenRouterMessage], tools:list, **kwargs) -> str | OpenRouterToolResponse: ...
     def _blocking_complete(self, messages: list[OpenRouterMessage], tools:list|None=None, **kwargs) -> str | OpenRouterToolResponse:
         tool_payload = {"tools": tools, "parallel_tool_calls": self.allow_parallel_tool_calls} if tools else {}
         payload = {"model": self.model, "messages": messages, **tool_payload, **kwargs}
@@ -111,7 +119,10 @@ class Model:
             raise ValueError(f"An error occurred while processing the response: '{data}'. {e}") from e
 
     # TODO: should request timeout be a setting rather than hardcoded?
-    # TODO: streaming with tools not handled...
+    @overload
+    def _streaming_complete(self, messages: list[OpenRouterMessage], tools:None=None, **kwargs) -> Generator[str, None, None]: ...
+    @overload
+    def _streaming_complete(self, messages: list[OpenRouterMessage], tools:list, **kwargs) -> Generator[str|OpenRouterToolResponse, None, None]: ...
     def _streaming_complete(self, messages: list[OpenRouterMessage], tools:list|None=None, **kwargs) -> Generator[str|OpenRouterToolResponse, None, None]:
         url = "https://openrouter.ai/api/v1/chat/completions"
         headers = {
@@ -240,21 +251,30 @@ attributes_map: dict[ModelName, Attr] = {{
 
 
 # from .model import Model, Message, Role
-# from typing import Literal, Generator, overload
 
-class Agent:
+
+# set up type-hinting such that if the user doesn't provide tools, then it's just str, not str|OpenRouterToolResponse
+class WithTools: ...
+class WithoutTools: ...
+HasTools = TypeVar('HasTools', WithTools, WithoutTools)
+# TODO: consider renaming to e.g. Chat or something similar, and reserve Agent for ReAct agents
+class Agent(Generic[HasTools]):
     """Basically just a model paired with message history tracking"""
+    @overload
+    def __init__(self: 'Agent[WithoutTools]', model: Model, tools:None=None): ...
+    @overload
+    def __init__(self: 'Agent[WithTools]', model: Model, tools:list): ...
     def __init__(self, model: Model, tools:list|None=None):
         self.model = model
         self.messages: list[OpenRouterMessage] = []
         self.tools = tools
 
     @overload
-    def add_message(self, *, role: Role, content: str): ...
+    def add_message(self: 'Agent[WithTools]|Agent[WithoutTools]', *, role: Role, content: str): ...
     @overload
-    def add_message(self, *, role: Role, content: str, tool_call_id: str): ...
+    def add_message(self: 'Agent[WithTools]', *, role: Role, content: str, tool_call_id: str): ...
     @overload
-    def add_message(self, *, role: Role, content: str, tool_calls: list[OpenRouterToolCall]): ...
+    def add_message(self: 'Agent[WithTools]', *, role: Role, content: str, tool_calls: list[OpenRouterToolCall]): ...
     def add_message(self, *, role: Role, content: str, tool_calls: list[OpenRouterToolCall]|None=None, tool_call_id: str|None=None):
         assert tool_calls is None or tool_call_id is None, "tool_calls and tool_call_id cannot both be provided"
         if tool_calls:
@@ -265,40 +285,57 @@ class Agent:
             message = OpenRouterMessage(role=role, content=content)
         self.messages.append(message)
     
-    def add_user_message(self, content: str):
+    def add_user_message(self: 'Agent[WithTools]|Agent[WithoutTools]', content: str):
         self.add_message(role='user', content=content)
 
-    def add_assistant_message(self, content: str):
+    def add_assistant_message(self: 'Agent[WithTools]|Agent[WithoutTools]', content: str):
         self.add_message(role='assistant', content=content)
     
-    def add_assistant_tool_calls(self, content: str, tool_calls: list[OpenRouterToolCall]):
+    def add_assistant_tool_calls(self: 'Agent[WithTools]', content: str, tool_calls: list[OpenRouterToolCall]):
         self.add_message(role='assistant', content=content, tool_calls=tool_calls)
 
-    def add_tool_message(self, tool_call_id: str, content: str):
+    def add_tool_message(self: 'Agent[WithTools]', tool_call_id: str, content: str):
         self.add_message(role='tool', tool_call_id=tool_call_id, content=content)
 
-    def add_system_message(self, content: str):
+    def add_system_message(self: 'Agent[WithTools]|Agent[WithoutTools]', content: str):
         self.add_message(role='system', content=content)
     
     @overload
-    def execute(self, stream:Literal[False]=False) -> str|OpenRouterToolResponse: ...
+    def execute(self:'Agent[WithTools]', stream:Literal[False]=False) -> str|OpenRouterToolResponse: ...
     @overload
-    def execute(self, stream:Literal[True]) -> Generator[str | OpenRouterToolResponse, None, None]: ...
-    def execute(self, stream:bool=False) -> str | OpenRouterToolResponse | Generator[str | OpenRouterToolResponse, None, None]:
+    def execute(self:'Agent[WithTools]', stream:Literal[True]) -> Generator[str | OpenRouterToolResponse, None, None]: ...
+    @overload
+    def execute(self:'Agent[WithoutTools]', stream:Literal[False]=False) -> str: ...
+    @overload
+    def execute(self:'Agent[WithoutTools]', stream:Literal[True]) -> Generator[str, None, None]: ...
+    def execute(self:'Agent[WithTools]|Agent[WithoutTools]', stream:bool=False) -> str | OpenRouterToolResponse | Generator[str | OpenRouterToolResponse, None, None]:
         if stream:
             return self._streaming_execute()
         else:
             return self._blocking_execute()
     
-    def _blocking_execute(self) -> str | OpenRouterToolResponse:
-        result = self.model.complete(self.messages, stream=False, tools=self.tools)
+    @overload
+    def _blocking_execute(self:'Agent[WithTools]') -> str | OpenRouterToolResponse: ...
+    @overload
+    def _blocking_execute(self:'Agent[WithoutTools]') -> str: ...
+    def _blocking_execute(self:'Agent[WithTools]|Agent[WithoutTools]') -> str | OpenRouterToolResponse:
+        # if here is mainly for type hinting since apparently it doesn't know how to merge the cases where self.tools is None|list
+        if self.tools is None:
+            result = self.model.complete(self.messages, stream=False)
+        else:
+            result = self.model.complete(self.messages, stream=False, tools=self.tools)
         if isinstance(result, str):
             self.add_assistant_message(result)
         else:
+            self = cast(Agent[WithTools], self) #TODO: is there a better way to narrow this
             self.add_assistant_tool_calls(result['thought'], result['tool_calls'])
         return result
 
-    def _streaming_execute(self) -> Generator[str|OpenRouterToolResponse, None, None]:
+    @overload
+    def _streaming_execute(self:'Agent[WithTools]') -> Generator[str|OpenRouterToolResponse, None, None]: ...
+    @overload
+    def _streaming_execute(self:'Agent[WithoutTools]') -> Generator[str, None, None]: ...
+    def _streaming_execute(self:'Agent[WithTools]|Agent[WithoutTools]') -> Generator[str|OpenRouterToolResponse, None, None]:
         # stream the chunks while also capturing them
         result_chunks = []
         tool_calls = []
@@ -311,6 +348,7 @@ class Agent:
         
         # add the message to the history after streaming is done
         if tool_calls:
+            self = cast(Agent[WithTools], self) #TODO: is there a better way to narrow this
             self.add_assistant_tool_calls(''.join(result_chunks), tool_calls)
         else:
             self.add_assistant_message(''.join(result_chunks))
